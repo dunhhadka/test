92d3c3863fd2567c3fb0819ee5fc1030d4bfe491
dunghv2 <dunghv2@sapo.vn>
2023-11-17 01:15:32 +0700

    first commit

---
+++ Project_Economic/src/main/java/com/example/project_economic/ProjectEconomicApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_economic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEconomicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEconomicApplication.class, args);
+    }
+
+}
