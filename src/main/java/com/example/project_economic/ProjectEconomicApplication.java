0919daafd016789093b44d0419624814fe95e638
dunghv2 <dunghv2@sapo.vn>
2023-11-17 01:40:07 +0700

    commit

--- src/main/java/com/example/project_economic/ProjectEconomicApplication.java
+++ src/main/java/com/example/project_economic/ProjectEconomicApplication.java
@@ -9,6 +9,7 @@ public class ProjectEconomicApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectEconomicApplication.class, args);
         System.out.println("hello");
+        System.out.println("adsfsdaf");
     }
 
 }
